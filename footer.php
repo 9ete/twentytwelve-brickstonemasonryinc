5b6b4626b5250a1c2046ae20baa7cfa906c0d8b6
hawkeye126 <pete.lower@gmail.com>
2014-09-04 14:28:56 -0700

    add prefooter met

--- footer.php
+++ footer.php
@@ -10,7 +10,31 @@
  */
 ?>
 	</div><!-- #main .wrapper -->
+
+	<section class='prefoot-meta'>
+		<div class='pfmeta-one pfmeta-block'>
+			Call Us: <?php echo do_shortcode( '[phonenumber]' ); ?>
+		</div>
+
+		<div class='pfmeta-two pfmeta-block'>
+			Email Us: <a href='mailto:ashley@brickstonemasonry.com'>ashley@brickstonemasonry.com</a>
+		</div>
+
+		<div class='pfmeta-three pfmeta-social pfmeta-block'>
+			<a href='http://facebook.com/brickstonemasonry'>
+				<img src='https://www.facebookbrand.com/img/assets/asset.f.logo.lg.png' width='30' />
+			</a>
+			<a href='http://instagram.com/brickstonemasonry'>
+				<img src='https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-xpa1/t39.2365-6/851582_417171855069447_55288290_n.png' width='30' />
+			</a>
+			<a href='http://linkedin.com/brickstonemasonry'>
+				<img src='https://www.facebookbrand.com/img/assets/asset.f.logo.lg.png' width='30' />
+			</a>
+		</div>
+	</section>
+
 </div><!-- #page -->
+
 
 <footer id="colophon" role="contentinfo">
 	<div class="site-info">
